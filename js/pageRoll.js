9ec69794bd65197d69f95e7434702a37095d9eb8
张志远 <1009070595@qq.com>
2021-05-05 17:53:50 +0800

    页面轮播

--- js/pageRoll.js
+++ js/pageRoll.js
@@ -1,5 +1,43 @@
 let oBox = document.getElementsByClassName("boxs");
-let oHtml = 60;
+// let oHtml = 60;
+
+// let app = new Vue({
+//     el: '#container',
+//     data: {
+        
+//         pageNo: [1, 2, 3, 4],
+//         currentPage: 0,
+//     },
+//     methods: {
+//         switchPage(pageNo) {
+//             // oBox[0].style.transition = "0.5s ease";
+//             oBox[0].style.top = -pageNo * oHtml + 'px';
+//             this.currentPage = pageNo;
+//             console.log(this.currentPage + ' ' + this.pageNo.length)
+
+//         }
+//     },
+// })
+
+// document.addEventListener("mousewheel", (event) => {
+//     if (event.wheelDelta < 0) {
+//         if (app.currentPage < app.pageNo.length - 1) {
+//             oBox[0].style.top = -app.currentPage * oHtml - oHtml + 'px';
+//             app.currentPage += 1;
+//             console.log(app.currentPage + ' ' + app.pageNo.length)
+//         }
+//     } else if (event.wheelDelta > 0) {
+//         if (app.currentPage > 0) {
+//             oBox[0].style.top = -app.currentPage * oHtml + oHtml + 'px';
+//             app.currentPage -= 1;
+//             console.log(app.currentPage + ' ' + app.pageNo.length)
+
+//         }
+//     }
+// })
+
+let oPages = document.getElementsByClassName("pages");
+let oHtml = 500;
 
 let app = new Vue({
     el: '#container',
@@ -11,7 +49,7 @@ let app = new Vue({
     methods: {
         switchPage(pageNo) {
             // oBox[0].style.transition = "0.5s ease";
-            oBox[0].style.top = -pageNo * oHtml + 'px';
+            oPages[0].style.top = -pageNo * oHtml + 'px';
             this.currentPage = pageNo;
             console.log(this.currentPage + ' ' + this.pageNo.length)
 
@@ -22,13 +60,13 @@ let app = new Vue({
 document.addEventListener("mousewheel", (event) => {
     if (event.wheelDelta < 0) {
         if (app.currentPage < app.pageNo.length - 1) {
-            oBox[0].style.top = -app.currentPage * oHtml - oHtml + 'px';
+            oPages[0].style.top = -app.currentPage * oHtml - oHtml + 'px';
             app.currentPage += 1;
             console.log(app.currentPage + ' ' + app.pageNo.length)
         }
     } else if (event.wheelDelta > 0) {
         if (app.currentPage > 0) {
-            oBox[0].style.top = -app.currentPage * oHtml + oHtml + 'px';
+            oPages[0].style.top = -app.currentPage * oHtml + oHtml + 'px';
             app.currentPage -= 1;
             console.log(app.currentPage + ' ' + app.pageNo.length)
 
